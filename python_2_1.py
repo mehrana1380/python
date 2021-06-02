9581933327be9c180d25d6108056799b108ca927
mehrana1380 <84845533+mehrana1380@users.noreply.github.com>
2021-06-02 00:20:48 -0700

    Create python_2_1.py

---
+++ python_2_1.py
@@ -0,0 +1,16 @@
+number_one = float(input("enter your first num: "))
+number_two = float(input("enter your second num: "))
+amaliat = input("nov amaliat ra moshakhas konid")
+
+if amaliat == '+':
+
+        print(number_one + number_two)
+elif amaliat == '-':
+
+        print(number_one - number_two)
+elif amaliat == '*':
+
+        print(number_one * number_two)
+elif amaliat == '%' :
+
+        print(number_one % number_two)
